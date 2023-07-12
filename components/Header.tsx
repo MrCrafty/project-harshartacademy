49daf683743f3c6d7f6f812f5adb234771069f87
hmithapara <hmithapara@horizontal.com>
2023-07-12 20:52:32 +0530

    changes in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,8 +3,10 @@ import { PiUserCircleThin } from "react-icons/pi";
 import { HiOutlineBars3BottomRight } from "react-icons/hi2";
 import { ReactNode, useState } from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 const Header = (): JSX.Element => {
-  const [IsLogin, setIsLogin] = useState(false);
+  const [IsLogin, setIsLogin] = useState(true);
+  const currentPage = useRouter().pathname;
   return (
     <header>
       <nav className="container mx-auto flex flex-row items-center justify-between">
@@ -20,30 +22,34 @@ const Header = (): JSX.Element => {
           </Link>
         </div>
         <div className="w-11/12 flex flex-row justify-between ml-10 text-lg">
-          <ul className=" hidden md:flex flex-row [&>li]:mr-5 [&>li]:transition-all [&>li]:duration-200 [&>li]:hover:cursor-pointer ">
+          <ul className=" hidden md:flex flex-row [&>li]:mr-5 [&>li]:transition-all [&>li]:duration-200 [&>li]:yhover:cursor-pointer ">
             <li className="hover:text-yellow-100">
-              <Link href="/">Home</Link>
+              <Link
+                href="/"
+                className={`${
+                  currentPage == "/" ? "underline underline-offset-8" : ""
+                }`}
+              >
+                Home
+              </Link>
             </li>
             <li className="hover:text-yellow-100">
-              <Link href="/courses">Courses</Link>
+              <Link
+                href="/courses"
+                className={`${
+                  currentPage == "/courses"
+                    ? "underline underline-offset-8"
+                    : ""
+                }`}
+              >
+                Courses
+              </Link>
             </li>
           </ul>
           <ul className=" flex flex-row  [&>li]:ml-5 [&>li]:transition-all [&>li]:duration-200 [&>li]:hover:cursor-pointer ">
-            <li
-              className={`hover:text-yellow-100 hidden md:${
-                IsLogin ? "hidden" : "block"
-              }`}
-            >
-              Login
-            </li>
-            <li
-              className={`hover:text-yellow-100 hidden md:${
-                IsLogin ? "block" : "hidden"
-              }`}
-            >
-              Logout
-            </li>
-            <li className={`hidden md:${IsLogin ? "block" : "hidden"}`}>
+            <li className={`hover:text-yellow-100`}>Login</li>
+            <li className={`hover:text-yellow-100 `}>Logout</li>
+            <li className={``}>
               <PiUserCircleThin className={`text-2xl`} />
             </li>
           </ul>
